e7c3385b6be97778c940db917f994afc997e9f23
Mensire <mensirer@gmail.com>
2021-10-08 20:02:52 +0300

    macros function

--- test/main.h
+++ test/main.h
@@ -9,4 +9,19 @@ int ret_str(va_list arg);
 int _putchar(char c);
 
 
+/**
+ * struct TypeStruct - The structure of our own defined type
+ *
+ * @t: Type to use
+ *
+ * @out: Function to handle the returns
+ */
 
+typedef struct TypeStruct
+{
+	char *t;
+
+	int (*out)(va_list);
+} TypeStruct;
+
+#endif
