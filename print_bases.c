97faae8e19e5c27816e272bd73926d2096f9533b
Mensire <mensirer@gmail.com>
2021-10-08 21:22:58 +0300

    changes

--- print_bases.c
+++ print_bases.c
@@ -44,41 +44,4 @@ int print_hex_big(va_list l, flags_t *f)
 	return (count);
 }
 
-/**
- * print_binary - prints a number in base 2
- * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
- * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
- * Return: the number of char printed
- */
-int print_binary(va_list l, flags_t *f)
-{
-	unsigned int num = va_arg(l, unsigned int);
-	char *str = convert(num, 2, 0);
 
-	(void)f;
-	return (_puts(str));
-}
-
-/**
- * print_octal - prints a number in base 8
- * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
- * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
- * Return: the number of char printed
- */
-int print_octal(va_list l, flags_t *f)
-{
-	unsigned int num = va_arg(l, unsigned int);
-	char *str = convert(num, 8, 0);
-	int count = 0;
-
-	if (f->hash == 1 && str[0] != '0')
-		count += _putchar('0');
-	count += _puts(str);
-	return (count);
-}
