60a95309d4efb4a393aef8d3a8193cbdf61846ea
Mensire <mensirer@gmail.com>
2021-10-08 21:30:43 +0300

    count digits

--- print_i.c
+++ print_i.c
@@ -60,4 +60,25 @@ void print_number(int n)
 	_putchar((n1 % 10) + '0');
 }
 
+/**
+ * count_digit - returns the number of digits in an integer
+ * for _printf
+ * @i: integer to evaluate
+ * Return: number of digits
+ */
+int count_digit(int i)
+{
+	unsigned int d = 0;
+	unsigned int u;
 
+	if (i < 0)
+		u = i * -1;
+	else
+		u = i;
+	while (u != 0)
+	{
+		u /= 10;
+		d++;
+	}
+	return (d);
+}
