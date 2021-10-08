38dad4461bbeb2c18a51576415db85b6e074b1eb
Mensire <mensirer@gmail.com>
2021-10-08 19:58:04 +0300

    delete

--- test/main.h
+++ test/main.h
@@ -1,4 +1,3 @@
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -10,19 +9,4 @@ int ret_str(va_list arg);
 int _putchar(char c);
 
 
-/**
- * struct TypeStruct - The structure of our own defined type
- *
- * @t: Type to use
- *
- * @out: Function to handle the returns
- */
 
-typedef struct TypeStruct
-{
-	char *t;
-
-	int (*out)(va_list);
-} TypeStruct;
-
-#endif
